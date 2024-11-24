c8e1bba9445ceafd4da088c360c1515a9295a4a2
kirrill <kirrillept@gmail.com>
2024-11-24 18:25:10 +0300

    Task4: End

--- sharding-repl-cache/scripts/mongo-count.sh
+++ sharding-repl-cache/scripts/mongo-count.sh
@@ -11,19 +11,61 @@ exit();
 EOF
 
 ###
-# Получаем кол-во записей на Шарде-1
+# Получаем кол-во записей на Шарде-1 БД-1
 ###
-echo "Кол-во документов на Шарде-1:"
-docker compose exec -T shard1_db2 mongosh --port 27019 --quiet <<EOF
+echo "Кол-во документов на Шарде-1 БД-1:"
+docker compose exec -T shard1_db1 mongosh --port 27018 --quiet <<EOF
 use somedb
 db.helloDoc.countDocuments()
 exit();
 EOF
 
 ###
-# Получаем кол-во записей на Шарде-2
+# Получаем кол-во записей на Шарде-1 БД-2
 ###
-echo "Кол-во документов на Шарде-2:"
+echo "Кол-во документов на Шарде-1 БД-2:"
+docker compose exec -T shard1_db2 mongosh --port 27019 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+
+###
+# Получаем кол-во записей на Шарде-1 БД-3
+###
+echo "Кол-во документов на Шарде-1 БД-3:"
+docker compose exec -T shard1_db3 mongosh --port 27020 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+
+###
+# Получаем кол-во записей на Шарде-2 БД-1
+###
+echo "Кол-во документов на Шарде-2 БД-1:"
+docker compose exec -T shard2_db1 mongosh --port 27021 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+###
+# Получаем кол-во записей на Шарде-2 БД-2
+###
+echo "Кол-во документов на Шарде-2 БД-2:"
+docker compose exec -T shard2_db2 mongosh --port 27022 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+###
+# Получаем кол-во записей на Шарде-2 БД-3
+###
+echo "Кол-во документов на Шарде-2 БД-3:"
 docker compose exec -T shard2_db3 mongosh --port 27023 --quiet <<EOF
 use somedb
 db.helloDoc.countDocuments()
